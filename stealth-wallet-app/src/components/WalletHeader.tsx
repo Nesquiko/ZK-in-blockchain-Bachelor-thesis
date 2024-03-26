f3f9c867647487f798cfac6027ea4cbc54a90ba5
Nesquiko <lukas.castven@gmail.com>
2024-03-26 17:56:11 +0100

    feat: fetch Bob's meta stealth address

--- stealth-wallet-app/src/components/WalletHeader.tsx
+++ stealth-wallet-app/src/components/WalletHeader.tsx
@@ -1,33 +1,53 @@
-import { Component, Match, Resource, Switch } from "solid-js";
-import { web3 } from "../lib/provider";
+import { Component, Match, Resource, Switch, createSignal } from "solid-js";
+import { formatWei, shortenAddress } from "../lib/format";
 
 interface WalletHeaderProps {
   name: string;
+  address: string;
   balance: Resource<bigint>;
   colorCls: string;
 }
 
 const WalletHeader: Component<WalletHeaderProps> = (props) => {
+  let copiedTimeout: NodeJS.Timeout | null = null;
+  const [copied, setCopied] = createSignal(false);
   return (
     <div class="flex justify-between text-xl">
       <h2 classList={{ [props.colorCls]: true }} class="rounded-full px-4 py-2">
-        {props.name}
+        <p class="text-black">{props.name}</p>
+        <p class="text-sm text-center text-gray-700 select-none">
+          {shortenAddress(props.address)}
+          <i
+            classList={{
+              "fa-copy": !copied(),
+              "fa-check text-lime-600": copied(),
+            }}
+            class="px-2 fa-solid fa-sm cursor-pointer"
+            onClick={() => {
+              navigator.clipboard.writeText(props.address);
+              setCopied(true);
+              if (!copiedTimeout) {
+                copiedTimeout = setTimeout(() => {
+                  setCopied(false);
+                  copiedTimeout = null;
+                }, 2000);
+              }
+            }}
+          ></i>
+        </p>
       </h2>
-      <h2 class="rounded-full px-4 py-2 bg-gray-300">
-        <Switch fallback={<span>0.0 ETH</span>}>
-          <Match when={props.balance.loading}>
-            <span>0.0 ETH</span>
-          </Match>
-          <Match when={props.balance.error}>
-            <span>-.- ETH</span>
-          </Match>
-          <Match when={props.balance()}>
-            {(balance) => (
-              <span>{web3.utils.fromWei(balance(), "ether")} ETH</span>
-            )}
-          </Match>
-        </Switch>
-      </h2>
+
+      <div class="flex items-center justify-center">
+        <p class="rounded-full px-4 py-2 bg-gray-300">
+          <Switch fallback={"0.0 ETH"}>
+            <Match when={props.balance.loading}>{"0.0 ETH"}</Match>
+            <Match when={props.balance.error}>{"-.- ETH"}</Match>
+            <Match when={props.balance()}>
+              {(balance) => `${formatWei(balance())} ETH`}
+            </Match>
+          </Switch>
+        </p>
+      </div>
     </div>
   );
 };
--- stealth-wallet-app/src/pages/BobWallet.tsx
+++ stealth-wallet-app/src/pages/BobWallet.tsx
@@ -1,18 +1,23 @@
 import { Component, For, Match, Show, Switch, createResource } from "solid-js";
 import {
-  web3,
   type StealthAddress,
   fetchBalance,
   fetchStealthAddresses,
   bobsPrimaryAccount,
   bobsSecondaryAccounts,
+  fetchMetaStealthAddres,
 } from "../lib/provider";
 import WalletHeader from "../components/WalletHeader";
 import { formatWei, shortenAddress } from "../lib/format";
 import WithdrawDialog from "../components/WithdrawDialog";
+import { Callout, CalloutTitle } from "../components/ui/callout";
 
 const BobsMainWallet: Component = () => {
   const [balance] = createResource(bobsPrimaryAccount, fetchBalance);
+  const [metaStealthAddress] = createResource(
+    bobsPrimaryAccount.address,
+    fetchMetaStealthAddres,
+  );
   const [stealthAddresses] = createResource(
     bobsPrimaryAccount,
     fetchStealthAddresses,
@@ -39,25 +44,54 @@ const BobsMainWallet: Component = () => {
 
   const stealthAddressesList = () => {
     return (
-      <div class="rounded-lg my-2 bg-violet-100 w-full">
-        <h1 class="text-lg p-2 rounded-t-lg bg-violet-300">
-          Bob's Stealth Addresses
-        </h1>
-        <div class="space-y-2 p-2">
-          <Switch fallback={<span>0.0 ETH</span>}>
-            <Match when={stealthAddresses.loading}>
-              <span>0.0 ETH</span>
-            </Match>
-            <Match when={stealthAddresses.error}>
-              <span>-.- ETH</span>
-            </Match>
-            <Match when={stealthAddresses()}>
-              {(addresses) => (
-                <For each={addresses()}>{stealthAddressesListItem}</For>
-              )}
-            </Match>
-          </Switch>
-        </div>
+      <div class="p-2">
+        <Switch
+          fallback={
+            <Callout variant="warning">
+              <CalloutTitle>No meta stealth address</CalloutTitle>
+            </Callout>
+          }
+        >
+          <Match when={metaStealthAddress.loading}>
+            <Callout>
+              <CalloutTitle>Fetching meta stealth address</CalloutTitle>
+            </Callout>
+          </Match>
+          <Match when={metaStealthAddress.error}>
+            <Callout variant="error">
+              <CalloutTitle>
+                Error while fetching meta stealth address
+              </CalloutTitle>
+            </Callout>
+          </Match>
+          <Match
+            when={
+              metaStealthAddress() !== undefined &&
+              metaStealthAddress()?.pubKey !== "0x"
+            }
+          >
+            <h1 class="text-lg p-2 rounded-t-lg bg-violet-300">
+              Bob's Meta Stealth Addresses
+            </h1>
+            <div class="rounded-b-lg bg-violet-100 w-full">
+              <div class="space-y-2 p-2">
+                <Switch fallback={<span>0.0 ETH</span>}>
+                  <Match when={stealthAddresses.loading}>
+                    <span>0.0 ETH</span>
+                  </Match>
+                  <Match when={stealthAddresses.error}>
+                    <span>-.- ETH</span>
+                  </Match>
+                  <Match when={stealthAddresses()}>
+                    {(addresses) => (
+                      <For each={addresses()}>{stealthAddressesListItem}</For>
+                    )}
+                  </Match>
+                </Switch>
+              </div>
+            </div>
+          </Match>
+        </Switch>
       </div>
     );
   };
@@ -67,10 +101,11 @@ const BobsMainWallet: Component = () => {
       <div class="max-w-xl mx-auto">
         <WalletHeader
           balance={balance}
+          address={bobsPrimaryAccount.address}
           name="Bob's Primary Wallet"
           colorCls="bg-violet-300"
         />
-        <div>{stealthAddressesList()}</div>
+        {stealthAddressesList()}
       </div>
     </div>
   );
