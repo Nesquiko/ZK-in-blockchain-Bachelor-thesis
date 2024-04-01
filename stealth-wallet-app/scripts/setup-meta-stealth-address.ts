79d714d5eb85cdeab73980b393cd055d77b7a6a9
Nesquiko <lukas.castven@gmail.com>
2024-04-01 11:15:51 +0200

    refactor: update setup script according to previous refactors

--- stealth-wallet-app/scripts/setup-meta-stealth-address.ts
+++ stealth-wallet-app/scripts/setup-meta-stealth-address.ts
@@ -10,16 +10,17 @@ const web3 = new Web3(new HttpProvider("http://127.0.0.1:8545"));
 
 const META_STEALTH_REGISTRY_ADDRESS =
   "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-// "0xd8D7bB864B099D437dB603728E90C1402faF3Bee" // on sepolia
+const secret = BOBS_SECRET;
 
-const PRIVATE_KEY = bobsPrimaryAccount.privateKey;
-const PUBLIC_KEY = privateToPublic(Buffer.from(strip0x(PRIVATE_KEY), "hex"));
-const HASH_OF_SECRET = "0x" + poseidon([BOBS_SECRET]).toString(16);
+const account = bobsPrimaryAccount;
+web3.eth.accounts.wallet.add(account);
+const privateKey = account.privateKey;
+const publicKey = privateToPublic(Buffer.from(strip0x(privateKey), "hex"));
+const hashOfSecret = "0x" + poseidon([secret]).toString(16);
 
 interface MetaStealthAddress {
   pubKey: Uint8Array;
-  h: string;
-  signature: Uint8Array;
+  secretHash: string;
 }
 
 const metaStealthRegistry = new web3.eth.Contract(
@@ -30,39 +31,19 @@ metaStealthRegistry.handleRevert = true;
 
 const dataToSign = web3.utils.keccak256(
   web3.utils.encodePacked(
-    { value: "0x" + Buffer.from(PUBLIC_KEY).toString("hex"), type: "bytes" },
-    { value: HASH_OF_SECRET, type: "bytes32" },
+    { value: "0x" + Buffer.from(publicKey).toString("hex"), type: "bytes" },
+    { value: hashOfSecret, type: "bytes32" },
   ),
 );
-const digest = web3.utils.keccak256(
-  web3.utils.encodePacked(
-    {
-      value: "\x19Ethereum Signed Message:\n32",
-      type: "string",
-    },
-    { value: dataToSign.slice(2), type: "bytes32" },
-  ),
-);
-
-const rawSignature = secp256k1.sign(digest.slice(2), PRIVATE_KEY.slice(2));
-const r = rawSignature.r.toString(16).padStart(64, "0");
-const s = rawSignature.s.toString(16).padStart(64, "0");
-const v = rawSignature.recovery! + 27;
-const signature = web3.utils.encodePacked(
-  { value: r, type: "bytes32" },
-  { value: s, type: "bytes32" },
-  { value: v, type: "uint8" },
-);
+const signature = account.sign(dataToSign);
 
 const metaStealthAddress: MetaStealthAddress = {
-  pubKey: PUBLIC_KEY,
-  h: HASH_OF_SECRET,
-  signature: Buffer.from(signature.slice(2), "hex"),
+  pubKey: publicKey,
+  secretHash: hashOfSecret,
 };
 
-const sender = web3.eth.accounts.wallet.add(PRIVATE_KEY).get(0);
 const txReceipt = await metaStealthRegistry.methods
-  .setMetaStealthAddress(metaStealthAddress)
-  .send({ from: sender?.address, gas: "1000000", gasPrice: "10000000000" });
+  .setMetaStealthAddress(metaStealthAddress, signature.signature)
+  .send({ from: account.address, gas: "1000000", gasPrice: "10000000000" });
 
 console.log(txReceipt);
